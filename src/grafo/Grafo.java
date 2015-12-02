36c749b190fdd5938148216fb4ecf50766541e8b
eaguilarg <eaguilarg@CC203.itam.mx>
2015-12-02 11:31:24 -0600

    faltan cosas

--- src/grafo/Grafo.java
+++ src/grafo/Grafo.java
@@ -3,6 +3,7 @@ package grafo;
 
 import java.util.ArrayList;
 import java.util.Queue;
+import java.util.Stack;
 
 
 public class Grafo<T extends Comparable> {
@@ -11,32 +12,44 @@ public class Grafo<T extends Comparable> {
         int [][]matrizAdyacencia;
         public boolean [] visitados;
         public T  [] elementos;
-        int numNodos=0;   
+        int Tope=0; 
+        int numElem=0;
         public NodoGrafo<T> origen;
     
         public void imprimeGrafo(){
             System.out.println("Elementos: ");
             imprimeElementos();
             System.out.println("");
+            System.out.println("");
             System.out.println("Visitados: ");
             imprimeVisitados();
+            System.out.println("");
+            System.out.println("");
             System.out.println("Matriz Adyacencia: ");
             imprimeMatriz();
+             System.out.println("");
                     
         }
+        
+        
         private void imprimeElementos(){
-            for(int i=0;i<elementos.length;i++)
+            for(int i=0;i<elementos.length;i++) 
                 System.out.print(elementos[i]+" ");
         }
         private void imprimeVisitados(){
             for(int i=0;i<visitados.length;i++)
-                System.out.print(visitados[i]+" ");
+                System.out.println(elementos[i]+") "+visitados[i]);
         }
         private void imprimeMatriz() {
-        for (int x=0; x <numNodos; x++) {
-            for (int y=0; y<numNodos; y++) {
-                if(y==numNodos)
+          System.out.print("  ");
+          imprimeElementos();
+          System.out.println("");
+        for (int x=0; x <Tope; x++) {
+            System.out.print(elementos[x]+" ");
+            for (int y=0; y<Tope; y++) {
+                if(y==Tope){
                     y=0;
+                }
                 System.out.print(matrizAdyacencia[x][y]+" ");
             }
             System.out.println("");
@@ -44,6 +57,7 @@ public class Grafo<T extends Comparable> {
        
         }
     
+        //constructor grafo, inicializar todo en cero
         public Grafo(int n){//n>0
             matrizAdyacencia=new int[n][n];
             visitados=new boolean [n];
@@ -57,22 +71,20 @@ public class Grafo<T extends Comparable> {
             for(int j=0; j<n;j++){
                 visitados[j]=false;
             }
-            numNodos=n;
+            Tope=n;
         }
         
-       
+       //funcion que busca la posicion en []elementos==> fila [][]ady
         private int buscarPos(NodoGrafo<T> buscar){
             int x=0;
-            T elem=elementos[x];
-            while(x<numNodos &&  elem.compareTo(buscar.getElem())!=0){
-                x++;
-                elem=elementos[x];
+            
+            while(x<numElem &&  elementos[x].compareTo(buscar.getElem())!=0){
+               x++;
             }
-            if(x==numNodos)//no existe
+            if(x==numElem)//no existe
                 return -1;
             else//si lo encontro
                 return x;
-            
             
         }
         
@@ -89,10 +101,13 @@ public class Grafo<T extends Comparable> {
         elementos=aux2;
     
         }
+        
+        //modifica []visitados (funcion para buscar orden)
         private void marcar(NodoGrafo<T> inicial){
             int pos=buscarPos(inicial);
             visitados[pos]=true;
         }
+        //funcion que verifica si elem []visitados ya fue visitado(true)
         private boolean marcado(NodoGrafo<T> inicial){
             boolean resp=false;
             int pos=buscarPos(inicial);
@@ -100,40 +115,105 @@ public class Grafo<T extends Comparable> {
                 resp=true;
             return resp;
         }
-        private Pila<T> obtenerAdyacencia(NodoGrafo<T> uno){
-            int pos= buscarPos(uno);
-            Pila<T> pila = null; 
+        //funcion que regresa cola de adyancencia de un nodo
+        public Stack<T> obtenerAdyacencia(NodoGrafo<T> uno){
+            int fila= buscarPos(uno);
+            Stack<T> cola = new Stack(); 
           
-            for(int i=0;i<elementos.length;i++){
-                if(matrizAdyacencia[pos][i]==1){
-                    pila.push(elementos[i]);
+            for(int columna=0;columna<numElem;columna++){
+                if(matrizAdyacencia[fila][columna]==1){
+                    cola.add(elementos[columna]);
                 }
                   
             }
-            return pila;
+            if(cola.size()==0){
+                return null;
+            }
+            return cola;
         
         }
-        public void DFS(T i){//recorrido por profundidad
-           NodoGrafo<T> inicio=new NodoGrafo<T>(i);
+        
+        //recorrido por profundidad
+        public Stack<T> DFS(T i, boolean done, Stack<T> pila){
+            NodoGrafo<T> inicio=new NodoGrafo(i);
             T vertice = null;
             NodoGrafo<T> v;
+            //Stack<T> cola=new Stack();
+        
             
             marcar(inicio);
-            System.out.println(inicio.getElem().toString());
-            Pila<T> adyacencia=obtenerAdyacencia(inicio);
-            while(!adyacencia.isEmpty())
-                 vertice=(T)adyacencia.pop();
-                 v=new NodoGrafo<T>(vertice);
-                 if(marcado(v)==false){
-                     DFS(vertice);
-                }
-                     
+           // cola.add(i);
+            done=done();
+            System.out.println(i.toString());
+            Stack<T> adyacencia=obtenerAdyacencia(inicio);
             
+            
+            if(done==true)
+                return pila;
+            
+            if(done==false && adyacencia==null){//no recorriste todo el grafo
+                int pos=done2();
+                pila.addElement(i);
+                return DFS(elementos[pos],done,pila);
+                
+            
+            }            
+            while(adyacencia!=null){
+                 vertice=adyacencia.pop();
+                 v=new NodoGrafo<T>(vertice);
+                  
+                 if(marcado(v)==false){
+                      return DFS(vertice,done,pila);
+                 }
+                  
+                 }
+                 
+            
+            }
+            
+           
+            
+            
+            return pila;
         }
+        
+        
+        
+        
+        
+     private boolean done(){
+         
+         int i=0;
+         while(i<numElem && visitados[i]==true){
+             i++;
+         }
+         if(i==numElem){//todo []visitados es true
+             return true;
+         }
+         else{ 
+             return false;
+         }
+                     
+     }
+     private int done2(){//busca pos []visitados = false
+         
+         int i=0;
+         while(i<numElem && visitados[i]==true){
+             i++;
+         }
+         if(i==numElem){//todo []visitados es true
+             return -1;
+         }
+         else{ 
+             return i;
+         }
+                     
+     }
+     
      
         
         
-        
+        //funcion que une dos elementos, modifica [][]ady
         public void join(T origen, T destino){
             NodoGrafo<T> o=new NodoGrafo(origen);
             NodoGrafo<T> d=new NodoGrafo(destino);
@@ -144,95 +224,50 @@ public class Grafo<T extends Comparable> {
                 matrizAdyacencia[x][y]=0;
             }else{
                 o.setSiguiente(d);
-                int x=buscarPos(o);
-                int y=buscarPos(d);
-                matrizAdyacencia[x][y]=1;
+                int fila=buscarPos(o);
+                int columna=buscarPos(d);
+                matrizAdyacencia[fila][columna]=1;
             }
     }
-       
-        public void insert(T elem, int contador){
-            //NodoGrafo<T> nuevo=new NodoGrafo(elem);
-                               
-               elementos[contador]=elem;
-                contador++;
-              
+       //funcion que agrega elem a []elementos
+        public void insert(T elem){
+            
+               if(numElem==Tope){
+                   expandCapacity();
+               }  
+               if(elem==null){
+                   return ;
+               }
                
+               elementos[numElem]=elem;
+               numElem++;
+              
         }
        
     
     
-    /*public String buscaOrden(NodoGrafo<T> inicial){
-        Pila pila=new Pila();
-        Queue cola = null;
-        boolean done=done();
-        
-        if(inicial.getElem()==null)
-            return "";
-        if(done==true)//si ya visitaste todo
-            return cola.toString();
-            
-        
-        int x=buscarPos(inicial);
-        int i=0;
-        while(i<size && matrizAdyacencia[x][i]==0){//mientras no encuentres conexion buscar en adyacencia
-              i++;
-        }
-        if(i==size){//nodo es final grafo
-             pila.push(inicial.getElem());
-             visitados[x]=true;
-             
-        }else{
-            cola.add(inicial.getElem());
-            visitados[x]=true;
-            
-        }
-        
-        
-     
-          
-        
-        
-        
-        return pila.toString();
-}
-    private boolean done(){
-        boolean resp=false;
-        int i=0;
-      while(visitados[i]==true && i<size){
-          i++;
-      }
-      if(i==size){
-          //todo elemento en visitados es true
-          resp=true;
-      }
-      return resp;
-      
-    }
     
-    */
     public static void main(String[] args) {
-        
-        
-       Grafo<Integer> g=new Grafo(5);
-       for(int i=1;i<=5;i++){
-           g.insert(i,0);
+        Stack<Integer> cola=new Stack<Integer>();
+        NodoGrafo<Integer>uno=new NodoGrafo(1);
+       Grafo<Integer> g=new Grafo(7);
+       for(int i=1;i<=g.Tope;i++){
+           g.insert(i);
        }
        g.imprimeGrafo();
     
-       g.join(1,2);
+       g.join(1,4);
        g.join(2,3);
-       g.join(2,4);
+       g.join(3,4);
        g.join(3,5);
-       g.join(4,6);
-       g.join(1,7);
-       g.join(7,8);
-       g.join(7,9);
-       g.join(8,10);
-       g.join(9,10);
-       
-      
-       g.DFS(1);
-       
+       g.join(5,6);
+       g.join(5,7);
+           
+     System.out.println("");
+      g.imprimeGrafo();
+     
+     System.out.println(g.DFS(1,false,cola).toString());
+     
     }
     
 }
